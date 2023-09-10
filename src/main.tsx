6a805021a363cc6ebac402051217e94ee4d9d30b
sujal lama <lsujal41@gmail.com>
2023-09-10 13:18:50 +0545

    aws-exports file changed

--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
 import { Amplify } from 'aws-amplify';
-import config from './aws-exports';
+import config from './aws-exports.js';
 Amplify.configure(config);
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
